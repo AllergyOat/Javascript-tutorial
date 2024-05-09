4da43018b1407ff573fa89861a611b0a555e88cc
AllergyOat <apichat.peanroop@gmail.com>
2024-05-09 14:22:03 +0700

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -89,20 +89,42 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFecth() {
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    return response.json()
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === "clothing"){
+        return new Clothing(productDetails);
+      }
+        return new Product(productDetails);
+    });
+
+    console.log('load products')
+  });
+
+  return promise;
+}
+
+/* loadProductsFecth().then(() => {
+  console.log('next step')
+});
+ */
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
-xhr.addEventListener('load', () => {
-  products = JSON.parse(xhr.response).map((productDetails) => {
-    if(productDetails.type === "clothing"){
-      return new Clothing(productDetails);
-    }
-      return new Product(productDetails);
-  });
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type === "clothing"){
+        return new Clothing(productDetails);
+      }
+        return new Product(productDetails);
+    });
 
-  console.log('load products')
+    console.log('load products')
 
-  fun();
+    fun();
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
